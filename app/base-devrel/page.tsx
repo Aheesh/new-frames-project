ecf0f9e160a349d24f8490b3e90a6d783f9f090b
Brian Doyle <brian.doyle@coinbase.com>
2024-04-25 15:57:50 -0400

    Test meet devrel

---
+++ app/base-devrel/page.tsx
@@ -0,0 +1,47 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from '../config';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'To the Sea',
+    },
+    {
+      label: 'Climb',
+    },
+    {
+      label: 'To The Mountains',
+    },
+  ],
+  image: {
+    src: `${NEXT_PUBLIC_URL}/base-devrel/mountain.webp`,
+    aspectRatio: '1:1',
+  },
+  state: { frame: 'start' },
+  postUrl: `${NEXT_PUBLIC_URL}/api/base-devrel`,
+});
+
+export const metadata: Metadata = {
+  title: 'Meet Base DevRel',
+  description: 'Follow us for the latest builder updates!',
+  openGraph: {
+    title: 'Meet Base DevRel',
+    description: 'Follow us for the latest builder updates!',
+    images: [`${NEXT_PUBLIC_URL}/base-devrel/mountain.webp`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Meet Base DevRel</h1>
+      <a href="https://docs.base.org/building-with-base/guides/hyperframes">
+        Click here to learn how to make this!
+      </a>
+    </>
+  );
+}
