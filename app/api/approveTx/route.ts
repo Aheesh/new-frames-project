b8abefe1433bb5c38bb2a0b11e700e9621930e9a
aheesh <aheesh@gmail.com>
2024-09-13 10:40:35 +0530

    Refactor console logs in approveTx route

--- app/api/approveTx/route.ts
+++ app/api/approveTx/route.ts
@@ -82,6 +82,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   //return new NextResponse('Approve', { status: 200 }); // TODO
   console.log('api/approveTx/route.ts : txData =>', txData);
+  console.log('api/approveTx/route.ts : text =>', text);
   //return NextResponse.json(txData);
   return new NextResponse(getHyperFrame(frame as string, text || '', message?.button));
 }
