9eb30d58d436ed6a60dfa6b79dbbb3e26a956175
aheesh <aheesh@gmail.com>
2024-09-16 16:58:41 +0530

    Remove postUrl assignment for Player-A in hyperframes

--- app/hyperframes.ts
+++ app/hyperframes.ts
@@ -92,7 +92,7 @@ addHyperFrame('Player-A', {
       aspectRatio: '1:1',
     },
     state: { frame: 'Player-A' },
-    postUrl: `${NEXT_PUBLIC_URL}/api/swapTx`,
+    //postUrl: `${NEXT_PUBLIC_URL}/api/swapTx`,
   }),
   1: 'approve',
   2: 'approve',
